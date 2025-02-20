123a5f40623b4664a3dc8e736b1aaa318ccafc02
malcomwoo <tadashimal@gmail.com>
2025-02-20 13:08:30 -0300

    Codigo finalizado - DependencyInversionViolation, GodClassViolation, HardCodedDependencies

--- DependencyInversionViolation.java
+++ DependencyInversionViolation.java
@@ -1,9 +1,3 @@
-/**
- *  Principio da inversao da dependencia enfatiza desacoplamento e abstracao.
- *  O principio consiste em dois conceitos nucleos: modulos de alto nivel nao devem depender sobre modulos de baixo nivel,
- *  e ambos devem depender sobre abstracoes.
- */
-
 public class DependencyInversionViolation {
     public static void main(String[] args) {
         LightBulb bulb = new LightBulb();
@@ -23,7 +17,6 @@ class LightBulb implements Switchable {
 }
 
 interface Switchable {
-    //criacao de uma interface de algo que liga e desliga
     public void turnOn();
     public void turnOff();
 }//fim da interface
@@ -31,15 +24,12 @@ interface Switchable {
 
 class Switch {
     private Switchable device;
-
     public Switch(Switchable device) {
         this.device = device;
     }
-
     public void turnOn() {
         this.device.turnOn();
     }
-
     public void turnOff() {
         this.device.turnOff();
     }
